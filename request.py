058f490f146d712ada794a633274a79c23645e6f
Angel Corredor <adcorredorm@unal.edu.co>
2020-10-28 02:39:38 -0500

    more info Done

--- request.py
+++ request.py
@@ -1,13 +1,13 @@
 import requests 
 import os
 import json
-import ghlinguist as ghl
 
 org_URL = 'https://api.github.com/orgs/uapa-team/repos'
 main_folder = 'clone_folder'
 
 def get_data(url):
-    return requests.get(url=url).json()
+    headers = {'Authorization': 'token 4fd92298adbc61efb4296a3cf8940db490d449cc'}
+    return requests.get(url=url, headers=headers).json()
 
 if __name__ == '__main__':
 
@@ -21,15 +21,15 @@ if __name__ == '__main__':
             'owner': repo['owner']['login'],
             'description': repo['description'],
             'branches_url': repo['branches_url'].split('{')[0],
-            'languages_url': repo['languages_url'],
             'contributors_url': repo['contributors_url'],
-            'commits_url': repo['commits_url'].split('{')[0],
             'clone_url': repo['clone_url'],
             'created': repo['created_at'],
             'updated': repo['updated_at'],
-            'language': repo['language'],
+            'branches': [],
             'default_branch': repo['default_branch'],
             'total_lines': 0,
+            'contributors': [],
+            'languages': [],
             'files': []
         }
     
@@ -38,9 +38,41 @@ if __name__ == '__main__':
 
     for repo in data:
         folder = '{}/{}'.format(main_folder, repo)
-        #os.system('git clone {} {}'.format(data[repo]['clone_url'], folder))
-        os.system('cd {}'.format(folder))
-        files = os.popen(f'git -C {folder} ls-files').read().split('\n')[:-1]
+
+        # Clonning the repository
+        os.system('git clone {} {}'.format(data[repo]['clone_url'], folder))
+
+        # Branches
+        branches = get_data(data[repo]['branches_url'])
+        for branch in branches:
+            commit = get_data(branch['commit']['url'])
+            data[repo]['branches'].append({
+                'name': branch['name'],
+                'protected': bool(branch['protected']),
+                'last_update': commit['commit']['committer']['date'],
+                'commit message': commit['commit']['message']
+            })
+        
+        # Contributors
+        contributors = get_data(data[repo]['contributors_url'])
+        for cont in contributors:
+            data[repo]['contributors'].append({
+                'username': cont['login'],
+                'url': cont['html_url'],
+                'contributions': int(cont['contributions'])
+            })
+        
+        # Languages
+        languages = os.popen(f'github-linguist {folder}').read().strip().split('\n')
+        for lang in languages:
+            p, l = lang.split('%')
+            data[repo]['languages'].append({
+                'name': l.strip(),
+                'percentage': float(p)
+                })
+
+        # Files
+        files = os.popen(f'git -C {folder} ls-files').read().strip().split('\n')
         for f in files:
             lines = int(os.popen('wc -l {}/{}/{}'.format(
                 main_folder, repo, f)).read().split(' ')[0])
@@ -49,21 +81,19 @@ if __name__ == '__main__':
 
             modifications = os.popen(f'git -C {folder} log --pretty="format:%ci" {f}').read().split('\n')
 
-            print(f, ghl.linguist(f'{folder}/{f}').language.name)
+            language = os.popen(f'github-linguist {folder}/{f}').read()
+            language = language.split('language:')[1].strip().split('\n')[0]
 
             file_info = {
                 'name': f,
                 'lines': lines,
                 'updates': len(modifications),
-                'last_update': modifications[0] if len(modifications) > 0 else None
+                'last_update': modifications[0] if len(modifications) > 0 else None,
+                'language': language
             }
             data[repo]['files'].append(file_info)
         
         break
-
-
-
-
     
     with open("data.json", "w+") as outfile:  
         json.dump(data, outfile, indent=2) 
